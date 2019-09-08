eeb7c69071fd0b79eb02b6e4c0cf9059d72597b8
Puja <waglepuja@gmail.com>
2019-09-08 16:22:35 -0400

    remove all items done

--- src/App.js
+++ src/App.js
@@ -1,129 +1,183 @@
-import React, {Component} from 'react';
-import './App.css';
-import firebase from './firebase';
-import Header from './Header';
+import React, { Component } from "react";
+import "./App.css";
+import firebase from "./firebase";
+import Header from "./Header";
 // import Main from './Main';
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
-    this.state={
+    this.state = {
       listType: "grocery",
       grocery: [],
       household: [],
       miscellaneous: [],
       pets: [],
-      userValue:"",      
+      userValue: ""
     };
   }
 
-  handleSelect = (event) =>{
+  handleSelect = event => {
     this.setState({
-      listType: event.target.value,
-    })
-  }
+      listType: event.target.value
+    });
+  };
 
-  handleInputChange=(event)=>{ 
+  handleInputChange = event => {
     this.setState({
-      userValue: event.target.value,       
-    })
-  }
+      userValue: event.target.value
+    });
+  };
 
-  handleSubmit=(event)=>{
-    // event.preventDefault(); 
+  handleSubmit = event => {
+    // event.preventDefault();
     // const newState=[];
     // newState.push(this.state.userValue);
     //   this.setState((prevState) => ({
     //   listItems: [...prevState.listItems, ...newState],
-    //   userValue:"",   
+    //   userValue:"",
     // })
     // )
     event.preventDefault();
-    const dbRef=firebase.database().ref(this.state.listType);
+    const dbRef = firebase.database().ref(this.state.listType);
     dbRef.push(this.state.userValue);
-    this.setState({userValue: ""});
-  }
-  
+    this.setState({ userValue: "" });
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     const dbRef = firebase.database().ref();
-    
-    dbRef.on('value', (data)=>{
+    // const sections = ["grocery", "household"];
+    dbRef.on("value", data => {
       const response = data.val();
-
+      // console.log(response);
       const newState = {};
       for (let key in response) {
-        // console.log(response,key);
-        newState[key] = Object.values({id: key,name:response[key]})
-
+        newState[key] = Object.values({ id: key, name: response[key] });
       }
-      console.log(newState);
-      this.setState(newState);
-    }); 
+
+      console.log("newState:", newState);
+
+      if (newState.household) {
+        this.formatToArray(newState.household[1], "household");
+      }
+
+      if (!newState.household) {
+        this.setState({
+          household: []
+        });
+      }
+
+      if (newState.grocery) {
+        this.formatToArray(newState.grocery[1], "grocery");
+      }
+      if (!newState.grocery) {
+        this.setState({
+          grocery: []
+        });
+      }
+
+      if (newState.pets) {
+        this.formatToArray(newState.pets[1], "pets");
+      }
+      if (!newState.pets) {
+        this.setState({
+          pets: []
+        });
+      }
+
+      if (newState.miscellaneous) {
+        this.formatToArray(newState.miscellaneous[1], "miscellaneous");
+      }
+      if (!newState.miscellaneous) {
+        this.setState({
+          miscellaneous: []
+        });
+      }
+      // this.setState(newState);
+    });
   }
 
-  removeListItem =(item)=>{
-    const dbRef=firebase.database().ref("grocery");
-    console.log("item", item);
-    dbRef.child(item).remove();    
-  }
-  
-  render(){
-    // const list= this.state[this.state.listType][1];
-
-    let list ={};
-    if(this.state[this.state.listType][1] !== undefined){
-      list=this.state[this.state.listType][1];
+  formatToArray = (typeObject, type) => {
+    const typeInfo = Object.entries(typeObject);
+    const data = typeInfo.map(item => ({
+      id: item[0],
+      item: item[1]
+    }));
+    console.log(data);
+    if (data[0]) {
+      this.setState({ [type]: data });
+    } else {
+      this.setState({ [type]: [] });
     }
-    
+  };
+
+  removeListItem = item => {
+    const dbRef = firebase.database().ref(this.state.listType);
+    console.log("item", item);
+    dbRef.child(item).remove();
+  };
+
+  render() {
     return (
       <div className="App">
-         <Header/>
-         {/* <Main/> */}
-        <header className="App-header">
+        <Header />
+        {/* <Main/> */}
+        {/* <header className="App-header">
           <h1>Grocery Time!</h1>
           <h2>Can't find your grocery list?</h2>
           <h3>no problem,we can help</h3>
-        </header>
-        <main className="mainContent">        
-        <form onSubmit={this.handleSubmit}>
-        
-        <div className="dropdown">
-        <h3>Pick your department</h3>
-        <label htmlFor="genre" className="visuallyHidden">Please Select</label>
-          <select onChange={this.handleSelect}>        
-            <option value="grocery">Grocery</option>
-            <option value="household">Household</option>
-            <option value="pets">Pets</option>
-            <option value="miscellaneous">Miscellaneous</option>
-          </select>
-          </div>
+        </header> */}
+        <main className="mainContent">
+          <form onSubmit={this.handleSubmit}>
+            <div className="dropdown">
+              <h3>Pick your department</h3>
+              <label htmlFor="genre" className="visuallyHidden">
+                Please Select
+              </label>
+              <select onChange={this.handleSelect}>
+                <option value="grocery">Grocery</option>
+                <option value="household">Household</option>
+                <option value="pets">Pets</option>
+                <option value="miscellaneous">Miscellaneous</option>
+              </select>
+            </div>
 
-          <input onChange={this.handleInputChange} type="text" value={this.state.userValue}/>
+            <input
+              onChange={this.handleInputChange}
+              type="text"
+              value={this.state.userValue}
+            />
           </form>
           <div>
-            <p>
-              {this.state.listType}
-            </p>
+            <p>{this.state.listType}</p>
             <ul>
-              
-              {Object.keys(list).map(id =>{
+              {/* {Object.keys(list).map(id =>{
                 console.log(list[id]);
+                console.log("id",id);
                 return(
-                  <li id={id} onClick={()=>this.removeListItem(id)}>{list[id]}</li>                  
+                  <li key={id} id={id} onClick={()=>this.removeListItem(id)}>{list[id]}</li>                  
                 ) 
                 
+              })} */}
+              {console.log(this.state[this.state.listType])}
+              {this.state[this.state.listType].map(item => {
+                return (
+                  <li
+                    key={item.id}
+                    id={item.id}
+                    onClick={() => this.removeListItem(item.id)}
+                  >
+                    {item.item}
+                  </li>
+                );
               })}
             </ul>
           </div>
         </main>
       </div>
-      );
+    );
   }
-  
-  
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,17 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
-class Header extends Component{
-    render(){
-        return (
-    <div className="App">
-        {/* <header className="App-header">
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
           <h1>Grocery Time!</h1>
           <h2>Can't remember where you left your grocery list?</h2>
           <h3>No problem, we do</h3>
-        </header> */}
-    </div>
-        )
-    }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Header;
